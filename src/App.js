e008f3bf3456e3549f443446b7dd799fea10b692
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-11-08 20:52:16 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let ingreso = prompt("Ingrese valor1")
+  let ingreso2 = prompt("Ingrese valor2")
+
   return (
     <div className="App">
+
+{( ingreso && ingreso2) && <p>La suma es {Number(ingreso) + Number(ingreso2)}</p> }
+
       <header className="App-header">
+        <h1>VOLVEMOS 20:20hs</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +25,14 @@ function App() {
         >
           Learn React
         </a>
+
+ <input type="text" />
+ <input type="password" />
+ <input type="range" />
+ <input type="date" />
+ <input type="checkbox" />
+ <input type="color" />
+ <input type="button"  placeholder='Boton' ></input> 
       </header>
     </div>
   );
